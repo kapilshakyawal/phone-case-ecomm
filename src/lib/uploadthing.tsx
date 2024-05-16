82046327f0aa24048382d08449126a2cb7c5cc83
Kapil Shakyawal <73704551+kapilshakyawal@users.noreply.github.com>
2024-05-16 15:26:52 +0530

    adding files

---
+++ src/lib/uploadthing.tsx
@@ -0,0 +1,3 @@
+import { OurFileRouter } from "@/app/api/auth/[kindeAuth]/uploadthing/core"
+import  {generateReactHelpers} from "@uploadthing/react"
+export const  {useUploadThing,uploadFiles} = generateReactHelpers<OurFileRouter>()
